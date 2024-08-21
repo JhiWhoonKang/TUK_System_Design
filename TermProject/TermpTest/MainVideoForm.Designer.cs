fba9b2f31594f1dadd8825df68bf7a3d0bb9f006
CHANMIN <1208cksals@naver.com>
2024-08-21 17:58:08 +0900

    찬민수정
    찬민수정

--- TermProject/TermpTest/MainVideoForm.Designer.cs
+++ TermProject/TermpTest/MainVideoForm.Designer.cs
@@ -29,24 +29,69 @@
         private void InitializeComponent()
         {
             this.PB_CAMERA = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PB_CAMERA)).BeginInit();
             this.SuspendLayout();
             // 
             // PB_CAMERA
             // 
-            this.PB_CAMERA.Location = new System.Drawing.Point(0, 0);
+            this.PB_CAMERA.Location = new System.Drawing.Point(702, 233);
             this.PB_CAMERA.Margin = new System.Windows.Forms.Padding(0);
             this.PB_CAMERA.Name = "PB_CAMERA";
-            this.PB_CAMERA.Size = new System.Drawing.Size(640, 480);
+            this.PB_CAMERA.Size = new System.Drawing.Size(428, 297);
             this.PB_CAMERA.TabIndex = 0;
             this.PB_CAMERA.TabStop = false;
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(28, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(629, 688);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(693, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "스테이지 초기화";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(693, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "카메라 시작";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(693, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "스테이지에 그리기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // MainVideoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(614, 467);
+            this.ClientSize = new System.Drawing.Size(1514, 926);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.PB_CAMERA);
             this.Name = "MainVideoForm";
             this.Text = "MainVideoForm";
@@ -60,5 +105,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox PB_CAMERA;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
